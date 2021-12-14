3be580fe51c4468296afaceea0a7fa1646fc94e3
Rosen Toshev <rtoscheff@gmail.com>
2021-12-14 22:23:21 +0200

    Added the artist section with a gradient layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,51 @@
+import { NextPageContext } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { Box, Text, Flex } from "@chakra-ui/layout";
+import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
+import { Image } from "@chakra-ui/react";
 
-const Home = () => {
-  return <div>home</div>;
+const Home = ({ artists }) => {
+  return (
+    <GradientLayout
+      roundImage
+      color="gray"
+      subtitle="profile"
+      title="Rosen Toshev"
+      description="15 public playlists"
+      image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
+    >
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex justify="space-between">
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image src="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2" borderRadius="100%" />
+              </Box>
+              <Box marginTop="20px">
+                <Text fontSize="large">{artist.name}</Text>
+                <Text fontSize="x-small">artist</Text>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
+    </GradientLayout>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+  console.log(artists);
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
